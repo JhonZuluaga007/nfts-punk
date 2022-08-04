d2994cc7387409f8aa8ee42e9e1d3bbd374c12b0
jhonzusa <jzuluaga@pastpost.com>
2022-08-03 23:29:33 -0500

    the necessary extensions for our smart contract are added, certain private methods are also adjusted to be able to mint and return the url of our image

--- contracts/NftsPunks.sol
+++ contracts/NftsPunks.sol
@@ -2,8 +2,57 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
-contract NftsPunks is ERC721 {
-    constructor() ERC721("NtfsPunks", "NPKS"){}
+contract NftsPunks is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
 
+    Counters.Counter private _idCounter;
+    uint256 public maxSupply;
+
+    constructor(uint256 _maxSupply) ERC721("NtfsPunks", "NPKS"){
+        maxSupply = _maxSupply;
+    }
+
+    function mint() public {
+        uint256 current = _idCounter.current();
+        require(current < maxSupply, "No Ntfs Punks left :(");
+        _safeMint(msg.sender, current);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns(string memory) {
+        require(_exists(tokenId), "ERC721 Metadata: URI query for nonexistent token");
+        
+        string memory jsonURI = Base64.encode(
+            abi.encodePacked(
+                '{ "name": "NftsPunks #',
+                tokenId,
+                '", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
+                " //TODO: Calculate image URL",
+                '"}'
+            )
+        );
+        
+        return string(abi.encodePacked("data:application/json;base64,", jsonURI));
+    }
+
+    // Override required
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
