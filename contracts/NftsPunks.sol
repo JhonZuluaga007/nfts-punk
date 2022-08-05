31a24a5cf375985214e00125368a781d2e9faf8d
jhonzusa <jzuluaga@pastpost.com>
2022-08-05 18:08:43 -0500

    the DNA of the cyberpunks is added and the main contract for the theme is organized

--- contracts/NftsPunks.sol
+++ contracts/NftsPunks.sol
@@ -6,12 +6,15 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+import "./PunksDNA.sol";
 
-contract NftsPunks is ERC721, ERC721Enumerable, Ownable {
+contract NftsPunks is ERC721, ERC721Enumerable, Ownable, PunkDNA {
     using Counters for Counters.Counter;
 
     Counters.Counter private _idCounter;
     uint256 public maxSupply;
+    mapping(uint256 => uint256) public tokenDNA;
+
 
     constructor(uint256 _maxSupply) ERC721("NtfsPunks", "NPKS"){
         maxSupply = _maxSupply;
@@ -20,18 +23,73 @@ contract NftsPunks is ERC721, ERC721Enumerable, Ownable {
     function mint() public {
         uint256 current = _idCounter.current();
         require(current < maxSupply, "No Ntfs Punks left :(");
+
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
+        _idCounter.increment();
         _safeMint(msg.sender, current);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://avataaars.io/";
+    }
+
+    function _paramsURI(uint256 _dna) internal view returns (string memory) {
+        string memory params;
+
+        // Params are intentionally scoped to avoid Too Deep Stack error
+        {
+            params = string(
+                abi.encodePacked(
+                    "accessoriesType=",
+                    getAccessoriesType(_dna),
+                    "&clotheColor=",
+                    getClotheColor(_dna),
+                    "&clotheType=",
+                    getClotheType(_dna),
+                    "&eyeType=",
+                    getEyeType(_dna),
+                    "&eyebrowType=",
+                    getEyeBrowType(_dna),
+                    "&facialHairColor=",
+                    getFacialHairColor(_dna),
+                    "&facialHairType=",
+                    getFacialHairType(_dna),
+                    "&hairColor=",
+                    getHairColor(_dna),
+                    "&hatColor=",
+                    getHatColor(_dna),
+                    "&graphicType=",
+                    getGraphicType(_dna),
+                    "&mouthType=",
+                    getMouthType(_dna),
+                    "&skinColor=",
+                    getSkinColor(_dna)
+                )
+            );
+        }
+
+        return string(abi.encodePacked(params, "&topType=", getTopType(_dna)));
+    }
+
+    function imageByDNA(uint256 _dna) public view returns (string memory) {
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+
+        return string(abi.encodePacked(baseURI, "?", paramsURI));
     }
 
     function tokenURI(uint256 tokenId) public view override returns(string memory) {
         require(_exists(tokenId), "ERC721 Metadata: URI query for nonexistent token");
+
+        uint256 dna = tokenDNA[tokenId];
+        string memory image = imageByDNA(dna);
         
         string memory jsonURI = Base64.encode(
             abi.encodePacked(
                 '{ "name": "NftsPunks #',
                 tokenId,
                 '", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
-                " //TODO: Calculate image URL",
+                image,
                 '"}'
             )
         );
